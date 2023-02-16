5345c274c81bcb365afef33fe88fb7c5af489a7a
lucasgonzalezgagliardo <lucas.e.gonzalez@gmail.com>
2023-02-16 09:21:50 +0100

    versio final

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,16 +10,13 @@ const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 router.get("/signin", isLoggedOut, (req, res, next) => {
-  //renderizar formulario de signin
   res.render("users/signin");
 });
 
 router.post("/signin", isLoggedOut, (req, res, next) => {
   
-  //recupero campos del formulario
   let {username, password, passwordRepeat} = req.body;
   
-  //comprobaciones de campos
   if(username == "" || password == "" || passwordRepeat == "") {
     res.render("users/signin", {mensajeError: "Faltan campos"});
     return;
@@ -32,15 +29,12 @@ router.post("/signin", isLoggedOut, (req, res, next) => {
   User.find({username})
   .then(results => {
     console.log("results ", results);
-    // if(results) {  //[{username: "Mariona", password: "1234"}]
     if(results.length != 0) {
-      //error
+      //
       res.render("users/signin", {mensajeError: "El usuario ya existe"});
       return;
     }
-    //el usuario ha pasado las validaciones
 
-    //proceso de encriptación con bcrypt:
     let salt = bcrypt.genSaltSync(saltRounds);
     let passwordEncriptado = bcrypt.hashSync(password, salt);
 
@@ -67,8 +61,7 @@ router.get("/login", isLoggedOut, (req, res, next)=> {
 router.post("/login", isLoggedOut, (req, res, next)=>{
   
   let {username, password} = req.body;
-  //username = "Mariona"
-  //password = "1234"
+
 
   if(username == "" || password == "") {
     res.render("users/login", { mensajeError: "Faltan campos" });
@@ -81,11 +74,7 @@ router.post("/login", isLoggedOut, (req, res, next)=>{
       res.render("users/login", { mensajeError: "Credenciales incorrectas" });
       return;
     }
-    // else if(results[0].password != password) {
-    //   res.render("users/login", { mensajeError: "Credenciales incorrectas" });
-    //   return;
-    // }
-    //results[0] = { username: "Mariona", password = "12312423452465356477356253414324313524524"}
+   
 
     if(bcrypt.compareSync(password, results[0].password)) {
       req.session.currentUser = username; //en req.session.currentUser guardamos la información del usuario que nos interese. Podemos guardar un string o un objeto con todos los datos
